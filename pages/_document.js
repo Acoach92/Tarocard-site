f2feee0cd698820cfce5408dbe328fb55f4a118f
Acoach92 <andrewcostella@hotmail.it>
2025-08-12 10:32:49 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,29 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="it">
+      <Head>
+        {/* Tailwind via CDN per evitare installazioni */}
+        <script src="https://cdn.tailwindcss.com" />
+        {/* Leaflet CSS (mappa) */}
+        <link
+          id="leaflet-style"
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        />
+      </Head>
+      <body className="bg-white text-neutral-900">
+        <Main />
+        <NextScript />
+        {/* Leaflet JS (mappa) */}
+        <script
+          id="leaflet-script"
+          src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+          async
+        />
+      </body>
+    </Html>
+  );
+}
 
